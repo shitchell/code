362258088a10eb0ef3cdf1ee9b6a5a2f103a4e68
shaun mitchell <shaun@shitchell.com>
2023-03-05 22:32:54 -0500

    revamped json stuff

--- sh/lib/text.sh
+++ sh/lib/text.sh
@@ -16,10 +16,10 @@ function case-insensitive-pattern() {
             # otherwise, just copy the character as-is
             insensitive_pattern="${insensitive_pattern}${char}"
 
-            if [ "${char}" = "[" ] && [ "${prev_char}" != "\\" ]; then
+            if [[ "${char}" == "[" && "${prev_char}" != "\\" ]]; then
                 # if we hit an unescaped '[' character, stop changing letters until we reach an unescaped ']' character
                 no_change=1
-            elif [ "${char}" = "]" ] && [ "${prev_char}" != "\\" ]; then
+            elif [[ "${char}" == "]" && "${prev_char}" != "\\" ]]; then
                 # we've reached an unescaped ']', so resume changing letters
                 no_change=0
             fi
@@ -45,6 +45,11 @@ function is-int() {
 # Return 0 if the specified string is a valid floating point number, 1 otherwise
 function is-float() {
     [[ "${1}" =~ ^[0-9]+\.[0-9]+$ ]]
+}
+
+# Return 0 if the specified string is a valid number (integer or floating point), 1 otherwise
+function is-number() {
+    is-int "${1}" || is-float "${1}"
 }
 
 # Remove leading/trailing whitespace from the specified string
@@ -86,20 +91,40 @@ function awk-csv() {
 ################################################################################
 
 # @description Escape a string for use in JSON, e.g. as a key or value
-# @usage json-escape <string>
+# @usage json-escape [--(no-)quotes] <string>
 # @attribution https://stackoverflow.com/a/29653643 https://stackoverflow.com/a/74426351/794241
 function json-escape() {
-    local string="${1}"
-    [[ -z "${string}" ]] && string=$(cat -)
-    [[ -z "${string}" ]] && return
+    local text=()
+    local do_quotes="" # false if empty
 
-    LANG=C awk '
+    # Parse arguments
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -q | --quotes)
+                do_quotes=true
+                shift
+                ;;
+            -Q | --no-quotes)
+                do_quotes=""
+                shift
+                ;;
+            *)
+                text+=("$1")
+                shift
+                ;;
+        esac
+    done
+
+    [[ -z "${text}" ]] && text=$(cat -)
+    [[ -z "${text}" ]] && return
+    
+    awk -v do_quotes="${do_quotes}" '
         BEGIN {
             for ( i = 1; i <= 127; i++ ) {
-                # Handle reserved JSON characters
+                # Handle reserved JSON characters and special characters
                 switch ( i ) {
-                    case 8: repl[ sprintf( "%c", i) ] = "\\b"; break
-                    case 9: repl[ sprintf( "%c", i) ] = "\\t"; break
+                    case 8:  repl[ sprintf( "%c", i) ] = "\\b"; break
+                    case 9:  repl[ sprintf( "%c", i) ] = "\\t"; break
                     case 10: repl[ sprintf( "%c", i) ] = "\\n"; break
                     case 12: repl[ sprintf( "%c", i) ] = "\\f"; break
                     case 13: repl[ sprintf( "%c", i) ] = "\\r"; break
@@ -110,9 +135,13 @@ function json-escape() {
             }
 
             for ( i = 1; i < ARGC; i++ ) {
-                s = ARGV[i]
-                # printf("%s", "\"")  # uncomment to surround with double quotes
+                if (i == 1 && do_quotes) {
+                    printf("\"")
+                } else if (i > 1) {
+                    printf(" ")
+                }
 
+                s = ARGV[i]
                 while ( match( s, /[\001-\037\177"\\]/ ) ) {
                     printf("%s%s", \
                         substr(s,1,RSTART-1), \
@@ -120,29 +149,96 @@ function json-escape() {
                     )
                     s = substr(s,RSTART+RLENGTH)
                 }
-                print s #"\""  # uncomment to surround with double quotes
+
+                printf("%s", s)
+                if (i == (ARGC - 1) && do_quotes) {
+                    printf("\"")
+                }
             }
             exit
         }
-    ' "${string}"
+    ' "${text[@]}"
 }
 
-# Convert an array with values in the format `key=value` to a JSON object
+# @description determine the type of a JSON value
+# @usage json-type <value>
+function json-type() {
+    local value="${1}"
+    local json_type
+
+    if [[ "${value}" == "null" ]]; then
+        type="null"
+    elif [[ "${value}" == "true" || "${value}" == "false" ]]; then
+        type="boolean"
+    elif [[ "${value}" =~ ^[0-9]+$ ]]; then
+        type="integer"
+    elif [[ "${value}" =~ ^[0-9]+\.[0-9]+$ ]]; then
+        type="float"
+    elif [[ "${value}" =~ ^\".*\"$ ]]; then
+        type="string"
+    elif [[ "${value}" =~ ^\[.*\]$ ]]; then
+        type="array"
+    elif [[ "${value}" =~ ^\{.*\}$ ]]; then
+        type="object"
+    else
+        type="unknown"
+    fi
+
+    echo "${type}"
+}
+
+# @description Convert an array with values in the format `key=value` to a JSON object
+# @usage json-map-from-keys [--detect-types] <key=value>...
 function json-map-from-keys() {
-    local values=("${@}")
+    local key_value_pairs=()
+    local detect_types=true
+
+    # Parse arguments
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -d | --detect-types)
+                detect_types=true
+                shift
+                ;;
+            -D | --no-detect-types)
+                detect_types=false
+                shift
+                ;;
+            *)
+                key_value_pairs+=("$1")
+                shift
+                ;;
+        esac
+    done
 
     # if no values given, check stdin
-    if [ ${#values[@]} -eq 0 ]; then
+    if [ ${#key_value_pairs[@]} -eq 0 ]; then
         while read -r line; do
-            values+=("${line}")
+            key_value_pairs+=("${line}")
         done
     fi
 
     local json='{'
-    for value in "${values[@]}"; do
-        local key=$(echo "${value}" | cut -d '=' -f 1 | json-escape)
-        local val=$(echo "${value}" | cut -d '=' -f 2- | json-escape)
-        json="${json}\"${key}\": \"${val}\", "
+    for key_value_pair in "${key_value_pairs[@]}"; do
+        local key="${key_value_pair%%=*}"
+        local value="${key_value_pair#*=}"
+        debug "key_value_pair: ${key} = ${value}"
+        
+        # Detect type if requested
+        if ${detect_types}; then
+            local json_type="$(json-type "${value}")"
+            case "${json_type}" in
+                "integer" | "float" | "boolean" | "null" | "array" | "object")
+                    value="${value}"
+                    ;;
+                "string" | "unknown")
+                    value=$(json-escape -q "${value}")
+                    ;;
+            esac
+        else
+            value=$(json-escape -q "${value}")
+        fi
+        json="${json}\"${key}\": ${value}, "
     done
     json="${json%, }}"
 
