28e8a33c3171bc202a57cefc9f36c0e8e9aa5f1b
shaun mitchell <shaun@shitchell.com>
2023-03-23 18:28:44 -0400

    clean up funcname formatting

--- sh/lib/debug.sh
+++ sh/lib/debug.sh
@@ -210,8 +210,9 @@ function _mini_debug() {
     if [[ "${DEBUG}" == "1" || "${DEBUG}" == "true" || -n "${DEBUG_LOG}" ]]; then
         timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
         prefix="\033[36m[${timestamp}]\033[0m "
-        prefix+="\033[35m$(basename "${BASH_SOURCE[-1]}"):"
-        prefix+="\033[1m${FUNCNAME[1]}:${BASH_LINENO[0]}\033[0m -- "
+        prefix+="\033[35m$(basename "${BASH_SOURCE[-1]}")"
+        [[ "${FUNCNAME[1]}" != "main" ]] && prefix+="\033[1m:${FUNCNAME[1]}()\033[0m"
+        prefix+="\033[32m:${BASH_LINENO[0]}\033[0m -- "
         printf "%s\n" "${@}" \
             | awk -v prefix="${prefix}" '{print prefix $0}' \
             | dd of="${DEBUG_LOG:-/dev/stderr}" conv=notrunc oflag=append status=none
