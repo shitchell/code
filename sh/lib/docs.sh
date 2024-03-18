fbf19390e64a74215a7caa7b4379d88d7a463c83
shaun mitchell <shaun@shitchell.com>
2024-03-18 00:35:10 -0400

    WIP: update lib documentation and begin adding options to allow for trimming space and separating pieces

--- sh/lib/docs.sh
+++ sh/lib/docs.sh
@@ -1,8 +1,171 @@
 #!/usr/bin/env bash
-: '
+: "
 This module provides functions for generating help documentation for shell
 libraries and functions based on Python-style docstrings.
-'
+
+# Example
+
+Docstrings will take the following format:
+
+    function foo() {
+        :  '[@summary] This is a brief <80 character summary
+            [@description] This is a description. The @description tag here is
+            optional; all lines of text from the 2nd line to the first explicit @tag
+            will be parsed as the description. Any leading/trailing whitespace will
+            be trimmed, so a blank line or two before/after *any* value is fine :)
+
+            @usage
+                [-h/--help] [-f/--foo <value>] <arg1> <arg2> [<arg3>]
+
+            @option -h
+                Show a brief help message
+
+            @option --help
+                Show a longer help message
+
+            @option -f/--foo <value>
+                Use <value> for the foo thingy
+
+            @arg <arg1>
+                This is a string that indicates the thing
+
+            @arg <arg2>
+                This is an integer which indicates this other thing
+
+            @optarg <arg3>
+                This is an optional string that indicates one last thing
+
+            @other-label
+                This is a label that does a thing
+
+            @stdin
+                This is what the function will read from stdin
+
+            @stdout
+                This is what the function will output
+
+            @stderr
+                This is what the function will output to stderr
+
+            @return 0
+                Successfully did the thing
+
+            @return 1
+                Something went wrong
+
+            @return 2
+                Could not find a file
+        '
+        echo 'hello world'
+    }
+
+Docstrings can use either single or double quotes.
+
+# Standard labels
+
+You can use create any tags you like, but a few specific tags are used by this
+library when generating documentation: 
+
+- @description: A description of the function. Any text from the 2nd line (the
+  line after the colon) until the first line with an explicit @label will be
+  parsed as the description.
+- @summary: A brief, <80 character summary of the function. Any text on the
+  first line (the same line as the colon) will be parsed as the summary unless
+  an explicit @label is used.
+- @usage: A compact, summarized list of all options the function accepts
+  *without* the function name included (see above).
+- @option: Describes a command line option and any parameters it accepts
+- @arg: Describes a required positional argument. Order matters! The first
+  occurence of '@arg' will be interpreted as the first positional argument, the
+  second will be interpreted as the second, etc...
+- @optarg: Describes an optional positional argument.
+- @stdin: Describes what the function will read from stdin.
+- @stdout: Describes what the function will output to stdout.
+- @stderr: Describes what the function will output to stderr.
+- @return: Describes an exit code.
+
+# Auto-generated labels
+
+There are two additional labels which get automatically generated for every
+function:
+
+- @uses_subshell: Boolean indicating whether the function runs in a subshell,
+  i.e.: whether it is defined with curly braces or parentheses.
+- @redirection: Any redirections defined after the closing brace/parenthesis of
+  the function. For most functions, this is empty.
+
+If you try to manually specify these labels in a docstring, they will be
+overwritten with their appropriate values as parsed. If you would like to give
+any additional information on the behavior associated with a function's
+redirections or running in a subshell, do so in the description or any other
+label as appropriate.
+
+# Behavior
+
+## General
+
+The parser will use the following pattern when extracting labels and their 
+values: /^\s*@([-A-Za-z0-9]+)\s+(.*)/, where the first capture group is the
+label and the second is its value. This basically translates to:
+
+- leading whitespace is ignored
+- it looks for an '@' symbol followed by letters, numbers, and/or dashes for the
+  label
+- it looks for one or more spaces after the label, and then everything else on
+  the line is parsed as the value
+
+Once a label has been defined, all subsequent lines will be added to its value,
+with leading/trailing whitespace trimmed. Line breaks follow OpenMarkdown rules:
+a single line break is replaced with a space, and two line breaks become a
+single newline.
+
+## Arrays and associative arrays
+
+Some labels like the description are expected to appear only once. Others are
+anticipated to appear multiple times, and some can be thought of as their own
+associative array, e.g.: exit codes and options. Because of the limitations of
+bash associative arrays, we handle these a little funkily. Bash associative
+arrays do not allow for storing anything other than strings as values, i.e.: we
+cannot store an array or an associative array inside another associative array.
+To that end, we use the ANSI Record Separator (0x1E) and Unit Separator (0x1F)
+characters (https://stackoverflow.com/a/18782271). For *any* label which is
+repeated, including single-use labels like @description, any re-occurence will
+yield a Record Separator in its value. For *specific* labels outlined below,
+their values will be parsed to produce a subsequent key and value to be
+separated by a Unit Separator character in the DOCSTRING associative array. As
+an example:
+
+    : '
+    @description This is a description
+    @option -h/--help: Show a helpful help message
+    @option -f/--foo <arg>: Sets foo to the arg
+    @return 0: Great success
+    '
+
+Will result in the following DOCSTRING values:
+
+    declare -A DOCSTRING=(
+        [description]='This is a description'
+        [option]=$'-h/--help\x1fShow a helpful help message\x1e-f/--foo <arg>\x1fSets foo to the arg'
+        [return]=$'0\x1fGreat success'
+    )
+
+The following labels will be produce Unit Record separators:
+
+  - @option
+  - @arg
+  - @optarg
+  - @return
+
+The keys/values of these labels can use either a colon (:) or newline (\n) to
+separate the key from the value:
+
+    : '
+    @option -h/--help: Show a helpful help message
+    @option -f/--foo <arg>
+      Sets foo to the arg
+    '
+"
 
 include-source 'debug.sh'
 
@@ -11,11 +174,12 @@ function generate-library-docs() {
     Generate help documentation for a library based on a docstring defined as
     the first line under its signature.
 
-    @usage       generate-library-docs <library_name>
-    @return     0 if the library was successfully documented
-    @return     1 general error
-    @return     2 if the library name was not provided
-    @return     3 if the library does not exist
+    @usage      generate-library-docs <library_name>
+    @stdout     The docstring as a "declare" statement
+    @return     0: if the library was successfully documented
+    @return     1: general error
+    @return     2: if the library name was empty
+    @return     3: if the library was not found
     '
     local lib_name="${1}"
     local lib_body lib_docstring
@@ -28,7 +192,9 @@ function generate-library-docs() {
     lib_body=$(declare -f "${lib_name}") || return 3
 
     # Extract the docstring
-    lib_docstring=$(__extract_docstring "${lib_body}") || return ${?}
+    lib_docstring=$(
+        __extract_docstring --trim-docstring --no-trim-lines "${lib_body}"
+    ) || return ${?}
 
     # Parse
     eval "$(__parse_docstring "${lib_docstring}")" || return ${?}
@@ -39,7 +205,7 @@ function generate-library-docs() {
 
 function generate-function-docstring() {
     : '
-    Generate help documentation for a function based on a doc string defined as
+    Generate help documentation for a function based on a docstring defined as
     the first line under its signature.
     
     @usage      <function_name>
@@ -183,14 +349,91 @@ function __extract_redirection() {
 function __extract_docstring() {
     :  'Extract the docstring from a function body.
 
-        @usage       <function_body>
-        @stdout      The docstring
-        @return      0 if the docstring was successfully extracted
-        @return      1 if the function body was not provided
-        @return      2 if the docstring was not found
+        @usage
+            [-t/--trim] [-T/--no-trim] [-l/--trim-lines] [-L/--no-trim-lines]
+            [-d/--trim-docstring] [-D/--no-trim-docstring]
+            [-s/--separate-summary] [-S/--no-separate-summary
+            <function_body>
+        @option -t/--trim
+            Trim leading/trailing whitespace from the docstring and each line
+        @option -T/--no-trim
+            Do not trim leading/trailing whitespace from the docstring or lines
+        @option -l/--trim-lines
+            Trim leading/trailing whitespace from each line, but not the
+            docstring
+        @option -L/--no-trim-lines
+            Do not trim leading/trailing whitespace from any lines
+        @option -d/--trim-docstring
+            Trim leading/trailing whitespace from the docstring
+        @option -D/--no-trim-docstring
+            Do not trim leading/trailing whitespace from the docstring
+        @option -s/--separate-summary
+            Insert a US (0x1F) character between the summary and the rest of the
+            docstring
+        @option -S/--no-separate-summary
+            Do not insert a US (0x1F) character between the summary and the rest
+            of the docstring
+        @arg <function_body>
+            The body of the function
+        @stdout
+            The docstring
+        @return 0
+            If the docstring was successfully extracted
+        @return 1
+            If the function body was not provided
+        @return 2
+            If a docstring was not found
     '
-    local f_body="${1}"
+    # Default values
+    local f_body
     local f_docstring
+    local do_separate_summary=true
+    local do_trim_line_whitespace=true
+    local do_trim_docstring_whitespace=true
+
+    # Parse the values
+    while [[ $# -gt 0 ]]; do
+        case "${1}" in
+            -s | --separate-summary)
+                do_separate_summary=true
+                shift 1
+                ;;
+            -S | --no-separate-summary)
+                do_separate_summary=false
+                shift 1
+                ;;
+            -t | --trim)
+                do_trim_line_whitespace=true
+                do_trim_docstring_whitespace=true
+                shift 1
+                ;;
+            -T | --no-trim)
+                do_trim_line_whitespace=false
+                do_trim_docstring_whitespace=false
+                shift 1
+                ;;
+            -l | --trim-lines)
+                do_trim_line_whitespace=true
+                shift 1
+                ;;
+            -L | --no-trim-lines)
+                do_trim_line_whitespace=false
+                shift 1
+                ;;
+            -d | --trim-docstring)
+                do_trim_docstring_whitespace=true
+                shift 1
+                ;;
+            -D | --no-trim-docstring)
+                do_trim_docstring_whitespace=false
+                shift
+                ;;
+            *)
+                f_body="${1}"
+                shift 1
+                ;;
+        esac
+    done
 
     # Ensure a function body was provided
     [[ -z "${f_body}" ]] && return 1
@@ -199,10 +442,12 @@ function __extract_docstring() {
     local line_num=0
     local quote="" # "'" or "\""
     local terminator="" terminator_regex="" terminator_found=false
-    while read -r line; do
-        # Remove leading/trailing whitespace
-        line="${line#"${line%%[![:space:]]*}"}"
-        line="${line%"${line##*[![:space:]]}"}"
+    while IFS=$'\n' read -r line; do
+        # Remove leading/trailing whitespace from the line
+        if ${do_trim_line_whitespace}; then
+            line="${line#"${line%%[![:space:]]*}"}"
+            line="${line%"${line##*[![:space:]]}"}"
+        fi
 
         # Increment the line number
         let line_num++
@@ -214,11 +459,17 @@ function __extract_docstring() {
         # If the line number is 3, ensure that it is the start of a docstring,
         # i.e. that it matches /^\s*:\s+['"]/
         if ((line_num == 3)); then
-            if [[ "${line}" =~ ^("( ")?":"[[:space:]]+(["'\""])(.*) ]]; then
+            if [[ "${line}" =~ ^[[:space:]]*("( ")?":"[[:space:]]+(["'\""])(.*) ]]; then
                 # Determine the type of quote used
                 quote="${BASH_REMATCH[2]}"
                 # Set the line to the part of the docstring after the quote
                 line="${BASH_REMATCH[3]}"
+                # If we are separating the summary out and this first line is
+                # not empty, then add a US (0x1F) character to the end of the
+                # line
+                if ${do_separate_summary} && [[ -n "${line}" ]]; then
+                    line+=$'\x1F'
+                fi
                 # Set the terminator to the quote + ";". Because double quoted
                 # strings can include escaped double quotes, we need to add a
                 # negative lookbehind to ensure that the quote is not escaped.
@@ -254,9 +505,11 @@ function __extract_docstring() {
         ${terminator_found} && break
     done <<< "${f_body}"
 
-    # Remove any leading/trailing whitespace
-    f_docstring="${f_docstring#"${f_docstring%%[![:space:]]*}"}"
-    f_docstring="${f_docstring%"${f_docstring##*[![:space:]]}"}"
+    # Remove any leading/trailing whitespace from the docstring
+    if ${do_trim_docstring_whitespace}; then
+        f_docstring="${f_docstring#"${f_docstring%%[![:space:]]*}"}"
+        f_docstring="${f_docstring%"${f_docstring##*[![:space:]]}"}"
+    fi
 
     # If the docstring was not found, return an error
     [[ -z "${f_docstring}" ]] && return 2
@@ -267,7 +520,7 @@ function __extract_docstring() {
 
 function __parse_docstring() {
     : '
-    Parse the docstring of a function.
+    Parse a docstring into an associate array of labels and values.
     
     The @description is optional. All lines until the first line matching
     /^\s*@/ are considered part of the description. The @usage and @return
@@ -275,7 +528,7 @@ function __parse_docstring() {
     which match the pattern "@{component} {value}" will have that component
     extracted and added to the output.
 
-    Output is a `declare` statement which can be `eval`ed to set a DOCSTRING
+    Output is a "declare" statement which can be evaluated to set a DOCSTRING
     associative array whose keys are the docstring and values are the
     corresponding values.
 
@@ -284,12 +537,51 @@ function __parse_docstring() {
     @return      1 if the docstring was not provided
     @return      2 if the docstring was invalid
     '
-    local docstring="${1}"
+    # Default values
+    local docstring
+    local trim_description=true
+    local trim_value_whitespace=true
     declare -A DOCSTRING=(
         [description]=""
         [usage]=""
         [return]=""
     )
+
+    # Parse the values
+    while [[ $# -gt 0 ]]; do
+        case "${1}" in
+            -t | --trim)
+                do_trim_line_whitespace=true
+                do_trim_docstring_whitespace=true
+                shift 1
+                ;;
+            -T | --no-trim)
+                do_trim_line_whitespace=false
+                do_trim_docstring_whitespace=false
+                shift 1
+                ;;
+            -l | --trim-lines)
+                do_trim_line_whitespace=true
+                shift 1
+                ;;
+            -L | --no-trim-lines)
+                do_trim_line_whitespace=false
+                shift 1
+                ;;
+            -d | --trim-docstring)
+                do_trim_docstring_whitespace=true
+                shift 1
+                ;;
+            -D | --no-trim-docstring)
+                do_trim_docstring_whitespace=false
+                shift
+                ;;
+            *)
+                docstring="${1}"
+                shift 1
+                ;;
+        esac
+    done
 
     if [[ -z "${docstring}" && -t 2 && ! -t 0 ]]; then
         debug "reading docstring from stdin"
@@ -303,14 +595,23 @@ function __parse_docstring() {
     fi
 
     # Extract the docstring
+    local is_first=true
     local component="description" component_value
-    while read -r line; do
+    while IFS=$'\n' read -r line; do
         debug "parsing line: '${line}'"
         # Remove leading/trailing whitespace
         line="${line#"${line%%[![:space:]]*}"}"
         line="${line%"${line##*[![:space:]]}"}"
 
-        if [[ "${line}" =~ ^"@"([A-Za-z]+)[[:space:]]*(.*) ]]; then
+        # If this is the first line, and it ends with a US (0x1F) character,
+        # then treat it as the summary and remove the US character
+        if ${is_first} && [[ "${line}" =~ $'\x1F'$ ]]; then
+            DOCSTRING[summary]="${line%$'\x1F'}"
+            is_first=false
+            continue
+        fi
+
+        if [[ "${line}" =~ ^[[:space:]]*"@"([A-Za-z]+)[[:space:]]*(.*) ]]; then
             # We've found a new component! Ensure the last component has no
             # leading/trailing whitespace and switch to the new component
             val="${DOCSTRING[${component}]}"
@@ -380,25 +681,29 @@ function test-func-subshell-double-redirect() (
 
 function test-func-single() {
     : '
-    @description This is a test function, and it
-                 includes newlines
+    This is a test function with a single quoted docstring.
+
     @usage       [-h] [--help] <arg1> [<arg2> ...]
+    @stdout      a simple greeting
+    @return      0: success, which this never returns. ever.
+    @return      1: always, no matter what
     '
+    echo "hello world"
     for i in {1..2}; do
         return ${i}
     done
 }
 
 function test-func-single-redirect() {
-    : "
-    @description This is a test function, and it
-                 includes newlines and multiple trixy escaped double quotes\";
+    : '
+    This is a test function with a single quoted docstring and redirection.
 
-    like this one:
-
-    \";
-
-    @usage       [-h] [--help] <arg1> [<arg2> ...]"; echo hi
+    @usage       [-h] [--help] <arg1> [<arg2> ...]
+    @stdout      a simple greeting
+    @return      0: success, which this never returns. ever.
+    @return      1: always, no matter what
+    '
+    echo "hello world"
     for i in {1..2}; do
         return ${i}
     done
