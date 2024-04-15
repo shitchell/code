b86ddf9f5bae284201a9047384468de0ba78e423
shaun mitchell <shaun@shitchell.com>
2024-04-15 10:43:31 -0400

    always return 0 or 1 based on DEBUG settings

--- sh/snippets/debug.sh
+++ sh/snippets/debug.sh
@@ -3,12 +3,16 @@
 function debug() {
     local prefix timestamp
     if [[ "${DEBUG}" == "1" || "${DEBUG}" == "true" || -n "${DEBUG_LOG}" ]]; then
-        timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
-        prefix="\033[36m[${timestamp}]\033[0m "
-        prefix+="\033[35m$(basename "${BASH_SOURCE[-1]}")"
-        [[ "${FUNCNAME[1]}" != "main" ]] && prefix+="\033[1m:${FUNCNAME[1]}()\033[0m"
-        prefix+="\033[32m:${BASH_LINENO[0]}\033[0m -- "
-        printf "%s\n" "${@}" \
-            | awk -v prefix="${prefix}" '{print prefix $0}' >> "${DEBUG_LOG:-/dev/stderr}"
+        if [[ ${#} -gt 0 ]]; then
+            timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
+            prefix="\033[36m[${timestamp}]\033[0m "
+            prefix+="\033[35m$(basename "${BASH_SOURCE[-1]}")"
+            [[ "${FUNCNAME[1]}" != "main" ]] && prefix+="\033[1m:${FUNCNAME[1]}()\033[0m"
+            prefix+="\033[32m:${BASH_LINENO[0]}\033[0m -- "
+            printf "%s\n" "${@}" \
+                | awk -v prefix="${prefix}" '{print prefix $0}' >> "${DEBUG_LOG:-/dev/stderr}"
+        fi
+        return 0
     fi
+    return 1
 }
