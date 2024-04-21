ae1ca58dc13d71d02fee69e6854d726d6eee4ec9
shaun mitchell <shaun@shitchell.com>
2024-04-20 21:45:14 -0400

    Change script permissions and update stdout and stderr indentation
    * Change file permissions to executable
    * Simplify stdout and stderr echo commands into a single line
    * Prefix stdout with "out:" and stderr with "err:"

--- sh/snippets/indent-stdout-stderr.sh
+++ sh/snippets/indent-stdout-stderr.sh
@@ -1,4 +1,3 @@
-{
-	echo "hello stdout"
-	echo "hello stderr" >&2
-} 1> >(sed 's/^/  /') 2> >(sed 's/^/  /' >&2)
+{ echo "hello stdout"; echo "hello stderr" >&2; } \
+    1> >(sed 's/^/  out: /') \
+    2> >(sed 's/^/  err: /' >&2)
