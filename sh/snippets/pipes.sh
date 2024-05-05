aa5e734cb1b0e461b4834dbd6d1a1e82ffd3cc7c
shaun mitchell <shaun@shitchell.com>
2024-05-04 20:27:55 -0400

    Add detailed file descriptor status display for pipe testing in bash script
    * Add comments explaining the purpose and sample output of the script.
    * Include conditions to check for pipe in and pipe out scenarios.
    * Provide a table summarizing the conditions and their meanings.
    * Explain the significance of `[[ -t 0 ]]`, `[[ -t 1 ]]`, and their negations in the context of pipe testing.

--- sh/snippets/pipes.sh
+++ sh/snippets/pipes.sh
@@ -1,4 +1,39 @@
 #!/usr/bin/env bash
+#
+# Display file descriptor statuses for pipe testing
+#
+# Sample output:
+#
+# $ ./pipes.sh               # no pipe in / no pipe out
+# : -t 0
+# : -t 1
+# : -t 2
+# $ ./pipes.sh | cat         # no pipe in / pipe out
+# : -t 0
+# : -t 2
+# : -p /dev/stdout
+# $ echo | ./pipes.sh | cat  # pipe in    / pipe out
+# : -t 2
+# : -p /dev/stdout
+# : -p /dev/stdin
+# $ echo | ./pipes.sh        # pipe in    / no pipe out
+# : -t 1
+# : -t 2
+# : -p /dev/stdin
+#
+# | condition          | pipe in | pipe out |
+# | ------------------ | ------- | -------- |
+# | [[ -t 0 ]]         | 1       | -        |
+# | [[ -t 1 ]]         | -       | 1        |
+# | [[ -t 2 ]]         | -       | -        |
+# | [ -p /dev/stdin ]  | 0       | -        |
+# | [ -p /dev/stdout ] | -       | 0        |
+#
+# tl;dr:
+# - [[ -t 0 ]]: stdin is not available
+# - [[ -t 1 ]]: not piping to another command
+# - ! [[ -t 0 ]]: stdin is available
+# - ! [[ -t 1 ]]: piping to another command
 
 [[ -t 0 ]] && echo ": -t 0"
 [[ -t 1 ]] && echo ": -t 1"
