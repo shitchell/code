314e58eb9c5c931053489e3c523f97db54dda2f8
shaun mitchell <shaun@shitchell.com>
2024-07-15 18:25:31 -0400

    actually exit when the audio file is over

--- wsl/bin/powerplay.ps1
+++ wsl/bin/powerplay.ps1
@@ -1,14 +1,28 @@
 #!/usr/bin/env psrun
 #
-# Play audio files through powershell
+# Play audio files through PowerShell
 
 Add-Type -AssemblyName PresentationCore
 
 $mediaPlayer = New-Object system.windows.media.mediaplayer
+
+# Trap for Ctrl+C to ensure proper cleanup
+Register-EngineEvent PowerShell.Exiting -Action {
+    Write-Host "" # Newline away from the while loop "..." dots
+    $mediaPlayer.Stop()
+    $mediaPlayer.Close()
+    $mediaEndedEvent.Set()
+} | Out-Null
+
+# Open and play the media file
 $mediaPlayer.open($args[0])
 $mediaPlayer.Play()
-while (!($mediaPlayer.MediaEnded)) {
-		Write-Host "Waiting for media player to exit"
+
+Write-Host -NoNewline "Waiting for media player to exit"
+while ($mediaPlayer.Position.TotalSeconds -ne $mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds) {
+    Write-Host -NoNewline "."
     Start-Sleep -Milliseconds 500
 }
+
 $mediaPlayer.Close()
+# Write-Host "Media playback ended."
