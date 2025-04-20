ba3dd8ce2ad400b833c1fe94a7a6649c041d3a82
shaun mitchell <shaun@shitchell.com>
2025-04-19 20:10:42 -0400

    updated example

--- js/snippets/hoisting_hell.js
+++ js/snippets/hoisting_hell.js
@@ -1,5 +1,14 @@
-const x = 1;
+// Works
+console.log("Setting `a` to `1` and then logging inside braces (works)");
+let a = 1;
 {
-  console.log(x); // ReferenceError
-  const x = 2;
+    console.log(`a => ${a}`);
+}
+
+// Doesn't work
+console.log("Setting `b` to `1`, logging its value, then setting it to `2` (fails)")
+let b = 1;
+{
+    console.log(b); // ReferenceError
+    let b = 2;
 }
