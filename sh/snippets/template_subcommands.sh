675c9a922f4411b2f3ffaba4bcea936c097ae9ef
shaun mitchell <shaun@shitchell.com>
2025-12-31 05:03:15 +0000

    bring up to date

--- sh/snippets/template_subcommands.sh
+++ sh/snippets/template_subcommands.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 #
 # This script does some stuff with subcommands
+#
+# A couple of solid guiding principles:
+# * **"One contiguous block"**: implementation should be designed such that
+#   adding any feasible, remotely possible future updates should only require
+#   updating a single contiguous block of code. If a future change would require
+#   updating multiple places, consider better design strategies.
+# * **Helpful functions are helpful**: create helper functions where it reduces
+#   the complexity of parent code, improves clarity (e.g.:
+#   `calculate-derivative ...` is easier to read and understand vs parsing and
+#   understanding several lines of code to do the same), removes duplicated
+#   logic, or even just the future potential of duplicated logic. Ideally, all
+#   functions (except help functions) are <50 lines. Also, favor long and
+#   explicit and self-documenting function names.
 
 
 ## imports #####################################################################
@@ -21,21 +34,19 @@ declare -ri E_INVALID_ACTION=3
 ## traps #######################################################################
 ################################################################################
 
-# @description Silence all output
-# @usage silence-output
 function silence-output() {
+    :  'Silence all script output'
     exec 3>&1 4>&2 1>/dev/null 2>&1
 }
 
-# @description Restore stdout and stderr
-# @usage restore-output
 function restore-output() {
+    :  'Restore script output after a call to `silence-output`'
     [[ -t 3 ]] && exec 1>&3 3>&-
     [[ -t 4 ]] && exec 2>&4 4>&-
 }
 
-# @description Exit trap
 function trap-exit() {
+    :  'An exit trap to restore output on script end'
     restore-output
 }
 trap trap-exit EXIT
@@ -47,9 +58,8 @@ trap trap-exit EXIT
 # Determine if we're in a terminal
 [[ -t 1 ]] && __IN_TERMINAL=true || __IN_TERMINAL=false
 
-# @description Set up color variables
-# @usage setup-colors
 function setup-colors() {
+    :  'Set up color variables'
     C_RED=$'\e[31m'
     C_GREEN=$'\e[32m'
     C_YELLOW=$'\e[33m'
@@ -64,13 +74,14 @@ function setup-colors() {
     S_BLINK=$'\e[5m'
     S_INVERT=$'\e[7m'
     S_HIDDEN=$'\e[8m'
+    
+    # {{TODO: ADD LABEL/CATEGORY COLORS HERE, e.g.: C_HEADER}}
 }
 
-# @description Unset color variables
-# @usage unset-colors
 function unset-colors() {
-    unset C_RED C_GREEN C_YELLOW C_BLUE C_MAGENTA C_CYAN C_WHITE \
-          S_RESET S_BOLD S_DIM S_UNDERLINE S_BLINK S_INVERT S_HIDDEN
+    :  'Unset color variables'
+    C_RED='' C_GREEN='' C_YELLOW='' C_BLUE='' C_MAGENTA='' C_CYAN='' C_WHITE=''
+    S_RESET='' S_BOLD='' S_DIM='' S_UNDERLINE='' S_BLINK='' S_INVERT='' S_HIDDEN=''
 }
 
 
@@ -78,45 +89,49 @@ function unset-colors() {
 ################################################################################
 
 function help-usage() {
+    :  'Print brief usage'
     # {{TODO: UPDATE USAGE}}
-    echo "usage: $(basename "${0}") [-h] <action> [<action args>]"
+    echo "usage: $(basename "${0}") [-h] [--help] [-c <when>] [-s] <action> [<args>]"
 }
 
 function help-epilogue() {
+    :  'Print a brief description of the script'
     # {{TODO: UPDATE EPILOGUE}}
     echo "run subcommands with individual options"
 }
 
 function help-full() {
+    :  'Print full help'
     help-usage
     help-epilogue
     echo
     echo "Actions:"
-    cat << EOF
+    cat << '    EOF'
     {{TODO: INSERT ACTIONS HERE}}
-    help                      display this help message
-EOF
+    help                      display help for a specific action
+    EOF
     echo
     echo "Base Options:"
-    cat << EOF
+    cat << '    EOF'
     -h                        display usage
     --help                    display this help message
     --config-file <file>      use the specified configuration file
-    -c/--color <on|auto|off>  whether to use color output
+    -c/--color <when>         when to use color ("auto", "always", "never")
     -s/--silent               suppress all output
     {{TODO: INSERT BASE OPTIONS HERE}}
-EOF
+    EOF
     echo
-    echo "For action specific options, run:"
-    echo "    $(basename "${0}") help <action>"
+    echo "For action-specific help, run:"
+    echo "    $(basename "${0}") <action> --help"
 }
 
 function parse-args() {
+    :  'Parse command-line arguments for the base command'
     # Parse the arguments first for a config file to load default values from
     CONFIG_FILE="${HOME}/.$(basename "${0}").conf"
     for ((i=0; i<${#}; i++)); do
         case "${!i}" in
-            -c | --config-file)
+            --config-file)
                 let i++
                 CONFIG_FILE="${!i}"
                 ;;
@@ -130,7 +145,7 @@ function parse-args() {
     DO_SILENT=false
     ACTION=""
     ACTION_ARGS=()
-    local color_when="${COLOR:-auto}" # auto, on, yes, always, off, no, never
+    local -- __color_when="${COLOR:-auto}" # auto, on, yes, always, off, no, never
 
     # Loop over the arguments
     while [[ ${#} -gt 0 ]]; do
@@ -148,13 +163,13 @@ function parse-args() {
                 shift 1
                 ;;
             -c | --color)
-                color_when="${2}"
+                __color_when="${2}"
                 shift 1
                 ;;
             -s | --silent)
                 DO_SILENT=true
                 ;;
-            # {{TODO: INSERT OPTIONS HERE}}
+            # {{TODO: INSERT BASE OPTIONS HERE}}
             -*)
                 echo "error: unknown option: ${1}" >&2
                 return ${E_INVALID_OPTION}
@@ -168,19 +183,8 @@ function parse-args() {
         shift 1
     done
 
-    # Any remaining arguments will be passed to the action function
-    while [[ ${#} -gt 0 ]]; do
-        case ${1} in
-            -h | --help)
-                __action-help "${ACTION}"
-                exit ${E_SUCCESS}
-                ;;
-            *)
-                ACTION_ARGS+=("${1}")
-                ;;
-        esac
-        shift 1
-    done
+    # Collect remaining arguments for the action
+    ACTION_ARGS=("${@}")
 
     # Ensure an action was specified
     if [[ -z "${ACTION}" ]]; then
@@ -194,7 +198,7 @@ function parse-args() {
 
     # Set up colors
     if ! ${DO_SILENT}; then
-        case "${color_when}" in
+        case "${__color_when}" in
             on | yes | always)
                 DO_COLOR=true
                 ;;
@@ -209,7 +213,7 @@ function parse-args() {
                 fi
                 ;;
             *)
-                echo "error: invalid color mode: ${color_when}" >&2
+                echo "error: invalid color mode: ${__color_when}" >&2
                 return ${E_ERROR}
                 ;;
         esac
@@ -225,69 +229,111 @@ function parse-args() {
 ## helpful functions ###########################################################
 ################################################################################
 
-# @description Do stuff
-# @usage do-stuff
+# {{TODO: REMOVE THIS AND ADD HELPER FUNCTIONS HERE}}
 function do-stuff() {
+    :  'Do stuff, optionally to an argument'
+    local -- __target="${1}"
+
     echo -n "i'm doin the stuff"
-    [[ -n "${1}" ]] && echo " to ${1}" || echo
+    if [[ -n "${__target}" ]]; then
+        echo " to ${C_CYAN}${__target}${S_RESET}"
+    else
+        echo
+    fi
 }
 
 
 ## action functions ############################################################
 ################################################################################
 
-# {{TODO: INSERT __action-* AND __help-* FUNCTIONS HERE}}
+### help action ################################################################
 
-# Just show the help
-function __help-help() {
+function __action-help-help() {
+    :  'Print help for the help action'
     echo "usage: $(basename "${0}") help [<action>]"
     echo
     echo "Display help for the specified action or for $(basename "${0}") if no"
     echo "action is specified."
 }
 
-function __action-help() {
-    local action="${1}"
+function __action-help-parse-args() {
+    :  'Parse arguments for the help action'
+    HELP_ACTION=""
 
-    # If an action was specified, show the help for that action
-    if [[ -n "${action}" ]]; then
-        local help_function="__help-${action}"
-        # Verify the help function exists
-        if type -t "${help_function}" &>/dev/null; then
-            # Call the help function and exit
-            "${help_function}"
+    while [[ ${#} -gt 0 ]]; do
+        case ${1} in
+            -h | --help)
+                __action-help-help
+                exit ${E_SUCCESS}
+                ;;
+            -*)
+                echo "error: unknown option: ${1}" >&2
+                return ${E_INVALID_OPTION}
+                ;;
+            *)
+                HELP_ACTION="${1}"
+                ;;
+        esac
+        shift 1
+    done
+
+    return ${E_SUCCESS}
+}
+
+function __action-help() {
+    :  'Display help for the specified action'
+    local -- __help_func
+
+    if [[ -n "${HELP_ACTION}" ]]; then
+        __help_func="__action-${HELP_ACTION}-help"
+        if type -t "${__help_func}" &>/dev/null; then
+            "${__help_func}"
         else
-            echo "error: no help found for action: ${action}" >&2
+            echo "error: no help found for action: ${HELP_ACTION}" >&2
             return ${E_INVALID_ACTION}
         fi
     else
-        # Otherwise, show the full help
         help-full
     fi
 }
 
-function __help-_test() {
-    echo "usage: $(basename "${0}") _test [-h]"
+
+### {{TODO: INSERT ACTIONS HERE}} #############################################
+
+### example action #############################################################
+
+function __action-example-help() {
+    :  'Print help for the example action'
+    echo "usage: $(basename "${0}") example [-m/--message <msg>] [<target>]"
     echo
-    echo "Do stuff."
+    echo "Do the example thing."
     echo
     echo "Options:"
     cat << '    EOF'
-    -h                       display usage
-    --help                   display this help message
-    -m/--message <msg>       the message to print
+    -h/--help            display this help message
+    -m/--message <msg>   the message to print (default: "hello world")
+    EOF
+    echo
+    echo "Arguments:"
+    cat << '    EOF'
+    <target>             optional target to do stuff to
     EOF
 }
 
-function __action-_test() {
+function __action-example-parse-args() {
+    :  'Parse arguments for the example action'
     # Default values
-    local message="hello world"
+    EXAMPLE_MESSAGE="hello world"
+    EXAMPLE_TARGET=""
 
-    # Loop over the arguments
     while [[ ${#} -gt 0 ]]; do
         case ${1} in
+            -h | --help)
+                __action-example-help
+                exit ${E_SUCCESS}
+                ;;
             -m | --message)
-                message="${2}"
+                EXAMPLE_MESSAGE="${2}"
                 shift 1
                 ;;
             -*)
@@ -295,14 +341,21 @@ function __action-_test() {
                 return ${E_INVALID_OPTION}
                 ;;
             *)
-                echo "error: unexpected argument: ${1}" >&2
-                return ${E_INVALID_OPTION}
+                EXAMPLE_TARGET="${1}"
                 ;;
         esac
         shift 1
     done
 
-    echo "${message}"
+    return ${E_SUCCESS}
+}
+
+function __action-example() {
+    :  'Run the example action'
+    echo "${EXAMPLE_MESSAGE}"
+    if [[ -n "${EXAMPLE_TARGET}" ]]; then
+        do-stuff "${EXAMPLE_TARGET}"
+    fi
 }
 
 
@@ -312,24 +365,28 @@ function __action-_test() {
 function main() {
     parse-args "${@}" || return ${?}
 
-    debug-vars ACTION ACTION_ARGS DO_COLORS DO_SILENT
+    debug-vars ACTION ACTION_ARGS DO_COLOR DO_SILENT
 
-    local exit_code=${E_SUCCESS}
-
-    # Find the action function
-    local action_func="__action-${ACTION}"
+    local -- __exit_code=${E_SUCCESS}
+    local -- __action_func="__action-${ACTION}"
+    local -- __parse_func="__action-${ACTION}-parse-args"
 
     # Verify the action function exists
-    if type -t "${action_func}" &>/dev/null; then
-        # Call the action function
-        "${action_func}" "${ACTION_ARGS[@]}"
-        exit_code=${?}
-    else
-        echo "error: no such action function found: ${ACTION}" >&2
-        exit_code=${E_INVALID_ACTION}
+    if ! type -t "${__action_func}" &>/dev/null; then
+        echo "error: unknown action: ${ACTION}" >&2
+        return ${E_INVALID_ACTION}
     fi
 
-    return ${exit_code}
+    # Parse action-specific arguments if a parser exists
+    if type -t "${__parse_func}" &>/dev/null; then
+        "${__parse_func}" "${ACTION_ARGS[@]}" || return ${?}
+    fi
+
+    # Call the action function
+    "${__action_func}"
+    __exit_code=${?}
+
+    return ${__exit_code}
 }
 
 
